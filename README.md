d748a7b314656412871c18b1e14d5e8fb76ed70b
Younes Aassila <47226184+younesaassila@users.noreply.github.com>
2022-08-18 14:48:18 +0200

    💄 Redesign website

---
+++ README.md
@@ -0,0 +1,5 @@
+# Download Twemoji
+
+Download a Twemoji in SVG or PNG.
+
+[https://download-twemoji.aassila.com](https://download-twemoji.aassila.com/)
--- app.js
+++ app.js
@@ -1,38 +1,40 @@
-const form = document.getElementById("form");
-const errorMessageElement = document.getElementById("error-message");
-const twemojiElement = document.getElementById("twemoji");
+const $ = (selector) => document.querySelector(selector);
+const form = $("#form");
+const emojiInputElement = $("#emoji-input");
+const formatSelectElement = $("#format-select");
+const errorMessageElement = $("#error-message");
+const twemojiElement = $("#twemoji");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   const formData = new FormData(form);
-
-  const emoji = formData.get("emoji-input").toString();
-  twemojiElement.textContent = emoji;
+  const emoji = formData.get("emoji-input");
+  const format = formatSelectElement.value || "svg";
+  const [extension, folder] = format.split("-");
+  twemojiElement.textContent = emoji ? emoji.trim() : "";
   try {
     twemoji.parse(twemojiElement, {
-      folder: "svg",
-      ext: ".svg",
+      folder: folder || extension,
+      ext: `.${extension}`,
     });
   } catch {
     return showError("The `twemoji` namespace is undefined.");
   }
   const twemojiImg = twemojiElement.querySelector("img");
-
-  if (!twemojiImg)
-    return showError("No emoji found or emoji not yet in Twemoji's database.");
-  if (!twemojiImg.src)
-    return showError("Could not retrieve the URL of the Twemoji's SVG.");
+  if (!twemojiImg || !twemojiImg.src)
+    return showError(`Unknown emoji '${emoji}'.`);
   showError(null);
-
-  window.open(twemojiImg.src, "_blank").focus();
+  const tab = window.open(twemojiImg.src, "_blank");
+  if (tab) tab.focus();
 });
 
 function showError(message) {
   if (message) {
+    emojiInputElement.setAttribute("aria-invalid", "true");
     errorMessageElement.textContent = message;
     errorMessageElement.style.display = "block";
   } else {
+    emojiInputElement.removeAttribute("aria-invalid");
     errorMessageElement.textContent = "";
     errorMessageElement.style.display = "none";
   }
